67863dd1441331e3586ecdb02c266d6a229aac90
nowm1006 <nowm1006@yahoo.co.jp>
2022-11-22 11:36:05 +0900

    added resolve function

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,29 @@
 import fs from 'node:fs';
 import path from 'node:path';
 const filename = process.argv[2];
-const basename = path.basename(filename);
+const basename = path.basename(filename, '.json');
 const outputname = basename + '-urls.txt';
 const content = fs.readFileSync(filename, { encoding: 'utf-8' });
 const plj = JSON.parse(content);
-const fd = fs.openSync(outputname, 'w');
+let fd = fs.openSync(outputname, 'w');
 Object.keys(plj.packages).forEach(packageName => {
     if (plj.packages[packageName].resolved) {
-        console.log(plj.packages[packageName].resolved);
         fs.writeFileSync(fd, plj.packages[packageName].resolved + '\n');
     }
 });
 fs.closeSync(fd);
+const dependencies = plj.dependencies;
+const resolveTree = resolveDeps(basename, {});
+fs.writeFileSync(basename + '-resolved.txt', JSON.stringify(resolveTree, null, 2));
+function resolveDeps(parent, tree) {
+    var _a;
+    const key = dependencies[parent].resolved;
+    const value = {};
+    if ((_a = dependencies[parent]) === null || _a === void 0 ? void 0 : _a.requires) {
+        Object.keys(dependencies[parent].requires).forEach((dep) => {
+            resolveDeps(dep, value);
+        });
+    }
+    tree[key] = value;
+    return tree;
+}
--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,33 @@ import fs from 'node:fs'
 import path from 'node:path'
 
 const filename = process.argv[2]
-const basename = path.basename(filename)
+const basename = path.basename(filename,'.json')
 const outputname = basename + '-urls.txt'
 
 const content = fs.readFileSync(filename,{encoding:'utf-8'})
 const plj = JSON.parse(content)
 
-const fd = fs.openSync(outputname,'w')
+let fd = fs.openSync(outputname,'w')
 Object.keys(plj.packages).forEach(packageName => {
     if (plj.packages[packageName].resolved) {
-        console.log(plj.packages[packageName].resolved)
         fs.writeFileSync(fd,plj.packages[packageName].resolved+'\n')
     }
 })
 fs.closeSync(fd)
+
+const dependencies = plj.dependencies
+const resolveTree:Record<string,any> = resolveDeps(basename,{})
+fs.writeFileSync(basename+'-resolved.txt',JSON.stringify(resolveTree,null,2))
+
+function resolveDeps(parent:string, tree:Record<string,any>):Record<string,any> {
+    const key = dependencies[parent].resolved
+    const value:Record<string,any> = {}
+    if (dependencies[parent]?.requires) {
+        Object.keys(dependencies[parent].requires).forEach((dep: string) => {
+            resolveDeps(dep, value)
+        })
+    }     
+    tree[key]=value
+
+    return tree
+}
