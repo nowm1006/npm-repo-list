959e63e17986e216841a82724d84f4b5d12b8e5e
nowm1006 <nowm1006@yahoo.co.jp>
2022-11-22 18:01:14 +0900

    added sorted list to install

--- dist/index.js
+++ dist/index.js
@@ -17,6 +17,11 @@ Object.keys(plj.packages).forEach(packageName => {
 // resolve dependencies
 const resolveTree = resolveDeps(plj.dependencies, basenameWithSlash, {});
 fs.writeFileSync(basedir + '/' + basename + '-resolved.txt', JSON.stringify(resolveTree, null, 2));
+// sort dependencies
+const sortedTree = sortDeps(plj.dependencies, basenameWithSlash, []);
+sortedTree.sort((a, b) => b.level - a.level);
+const myWritableStream = fs.createWriteStream(path.join(basedir, basename + '-sorted.txt'));
+myWritableStream.write(JSON.stringify(sortedTree, null, 2));
 function resolveDeps(data, parent, tree) {
     var _a;
     const key = data[parent].resolved;
@@ -29,10 +34,24 @@ function resolveDeps(data, parent, tree) {
     tree[key] = value;
     return tree;
 }
+function sortDeps(data, parent, list, level = 1) {
+    var _a;
+    const url = data[parent].resolved;
+    const item = { level, url };
+    list.push(item);
+    if ((_a = data[parent]) === null || _a === void 0 ? void 0 : _a.requires) {
+        Object.keys(data[parent].requires).forEach((dep) => {
+            sortDeps(data, dep, list, level + 1);
+        });
+    }
+    return list;
+}
 function downloadByUrl(url, saveDir) {
     const filename = path.basename(url);
-    fs.mkdirSync(saveDir, { recursive: true });
-    const outfile = fs.createWriteStream(saveDir + '/' + filename);
+    if (!fs.existsSync(saveDir)) {
+        fs.mkdirSync(saveDir);
+    }
+    const outfile = fs.createWriteStream(path.join(saveDir, filename));
     https.get(url, res => {
         res.pipe(outfile);
         res.on('end', () => {
